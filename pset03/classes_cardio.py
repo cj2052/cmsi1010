ba3aa7af2a50666b51e095233e156b60fcfc9959
Claire Butcher <clairejbutcher@gmail.com>
2025-06-25 12:35:54 -0700

    finished classes cardio

--- pset03/classes_cardio.py
+++ pset03/classes_cardio.py
@@ -1,33 +1,9 @@
-# ----------------------------------------------------------------------
-# This is the file classes_cardio.py
-#
-# The intent is to give you practice with classes.
-#
-# Complete the functions below.
-#
-# There are no stubs in this file, so you will need to infer exactly
-# how to write each class from the descriptions and the unit tests at
-# the bottom of the file. To run the tests, you can use the command
-#
-#     python3 -m unittest data_structures_cardio.py
-#
-# (or python depending on your system).
-#
-# Remove this comment, and other comments serving as instructions,
-# prior to submission. You can, and should, add your own comments,
-# but please remove all the comments that are here now.
-# ----------------------------------------------------------------------
 
 import math
 import io
 from contextlib import redirect_stdout
 import unittest
 
-
-# Write a class called Rectangle, with attributes with and height,
-# and methods to calculate the area and perimeter. The class should
-# also have a __str__ method that returns a string representation
-# of the rectangle in the format "Rectangle(width x height)".
 
 class Rectangle:
     def __init__(self, width, height):
@@ -44,11 +20,6 @@ class Rectangle:
         return f"Rectangle({self.width} x {self.height})"
 
 
-# Write a class called Circle, with an attribute radius,
-# and methods to calculate the area and circumference. The class
-# should also have a __str__ method that returns a string
-# representation of the circle in the format "Circle(radius=radius)".
-
 class Circle: 
     def __init__(self, radius):
         self.radius = radius
@@ -62,12 +33,6 @@ class Circle:
     def __str__(self):
         return f"Circle(radius={self.radius})"
 
-# Write a class called Song, with attributes title, artist, and
-# duration. The duration should be in seconds. The class should
-# have a __str__ method that returns a string representation
-# of the song in the format "title by artist (duration)s". The class
-# should also have a method called play that PRINTS "Playing title
-# by artist (duration)s".
 
 class Song: 
     def __init__(self, title, artist, duration):
@@ -80,13 +45,6 @@ class Song:
     
     def play(self):
         print(f"Playing {self.title} by {self.artist} ({self.duration}s)")
-    
-# Write a class called Playlist, which contains a list of Song
-# objects. The class should have methods to add a song, play all
-# songs, and a __str__ method that returns a string representation
-# of the playlist, with each song represented as "title by artist
-# (duration)s" and separated by a pipe character (|). If the playlist
-# is empty, the __str__ method should return "Playlist is empty.".
 
 class Playlist:
     def __init__(self):
@@ -96,18 +54,17 @@ class Playlist:
         self.songs.append(song)
     
     def play_all(self):
-        return self.songs[::]
+        for song in self.songs: #cycles through songs and plays each of them from play method in Song class
+            song.play() 
     
     def __str__(self):
+        playlist = []
         for song in self.songs[::]:
-            return f"{Song.title} by {Song.artist} ({Song.duration}s) | "
+            playlist.append(f"{song.title} by {song.artist} ({song.duration}s)") #adds songs to playlist
         if self.songs[::] == []:
             return "Playlist is empty."
+        return "|". join(playlist) #joings songs together and returns a full playlist
 
-
-
-# Keep the following tests in your file. Use them as you do the
-# work in this assignment.
 
 class TestClassesCardio(unittest.TestCase):
     def test_rectangle(self):
